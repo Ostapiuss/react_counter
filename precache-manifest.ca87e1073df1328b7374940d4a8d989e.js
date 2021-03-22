bf0c4fc5b9f931696f465bd4bcf9bee97cc6ae18
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-22 14:28:25 +0200

    Updates

---
+++ precache-manifest.ca87e1073df1328b7374940d4a8d989e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09529d2298bb92b302f9455bdadee00e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a8c02ccca2b43c0ee683",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "7c7a19eaf806aa4e071d",
+    "url": "./static/js/2.cad20d42.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cad20d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8c02ccca2b43c0ee683",
+    "url": "./static/js/main.81a62ff4.chunk.js"
+  },
+  {
+    "revision": "2a286f7172708a7e8c5a",
+    "url": "./static/js/runtime-main.3a2c9fb2.js"
+  }
+]);
